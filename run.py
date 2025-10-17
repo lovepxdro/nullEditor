2f7d22da9647752d5d32bad9374829c3672bdf86
lovepxdro <pedroantoniomergulhao@gmail.com>
2025-10-17 13:56:12 -0300

    feat: create layout for main window

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+# alterar apenas para os imports necessarios
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,6 +11,29 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("nullEditor")
         self.resize(1200, 720)
         self.center_on_screen()
+
+        main_splitter = QSplitter(Qt.Orientation.Vertical)
+        top_splitter = QSplitter(Qt.Orientation.Horizontal)
+        panel_style = "border: 1px solid #333;"
+        
+        editor_placeholder = QLabel("Área do Editor")
+        editor_placeholder.setStyleSheet(panel_style)
+
+        file_tree_placeholder = QLabel("Área da Árvore de Arquivos")
+        file_tree_placeholder.setStyleSheet(panel_style)
+        
+        terminal_placeholder = QLabel("Área do Terminal")
+        terminal_placeholder.setStyleSheet(panel_style)
+
+        top_splitter.addWidget(editor_placeholder)
+        top_splitter.addWidget(file_tree_placeholder)
+        top_splitter.setSizes([840, 360])
+
+        main_splitter.addWidget(top_splitter)
+        main_splitter.addWidget(terminal_placeholder)
+        main_splitter.setSizes([540, 180])
+
+        self.setCentralWidget(main_splitter)
 
         icon_path = "src/resources/icons/app_icon.png"
         self.setWindowIcon(QIcon(icon_path))
